e9ed02c0d6d423d2536792d3fcb6d80e6bad1f10
Nik Reiman <nik.reiman@gmail.com>
2013-03-15 22:28:36 +0100

    Add FrequencyCalculator to project

---
+++ Source/FrequencyCalculator.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    FrequencyCalculator.h
+    Created: 15 Mar 2013 10:27:10pm
+    Author:  nik
+
+  ==============================================================================
+*/
+
+#ifndef __FREQUENCYCALCULATOR_H_95E9AA40__
+#define __FREQUENCYCALCULATOR_H_95E9AA40__
+
+
+
+
+
+#endif  // __FREQUENCYCALCULATOR_H_95E9AA40__
