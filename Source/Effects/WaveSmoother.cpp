b402b73a50942aeda3e5a900df50ff930a03901e
Nik Reiman <nik.reiman@gmail.com>
2013-03-15 21:35:12 +0100

    Move algorithm over from old source code

--- Source/Effects/WaveSmoother.cpp
+++ Source/Effects/WaveSmoother.cpp
@@ -8,4 +8,12 @@
   ==============================================================================
 */
 
+#include <math.h>
 #include "WaveSmoother.h"
+
+void WaveSmoother::process(float* buffer, int numSamples)
+{
+    for(int i = 0; i < numSamples; ++i) {
+        buffer[i] = cos(M_PI * index++ / length) * ((base - target) / 2.0f) + ((base + target) / 2.0f);
+    }
+}
--- Source/Effects/WaveSmoother.h
+++ Source/Effects/WaveSmoother.h
@@ -11,8 +11,27 @@
 #ifndef __WAVESMOOTHER_H_1489CD73__
 #define __WAVESMOOTHER_H_1489CD73__
 
+#include "../BufferProcessor.h"
 
+class WaveSmoother : public BufferProcessor {
+public:
+    WaveSmoother() : BufferProcessor(), base(0.0f), target(0.0f), index(0), length(0) {}
+    virtual ~WaveSmoother() {}
 
+    void process(float* buffer, int numSamples);
+
+    void setBase(float inBase) { base = inBase; }
+    void setTarget(float inTarget) { target = inTarget; }
+    void setIndex(int inIndex) { index = inIndex; }
+    void setLength(int inLength) { length = inLength; }
+
+private:
+    float base;
+    float target;
+
+    int index;
+    int length;
+};
 
 
 #endif  // __WAVESMOOTHER_H_1489CD73__
