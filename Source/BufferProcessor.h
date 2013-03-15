5648d497c01cfc937cdec07f1ab0d23cc4c6d1c7
Nik Reiman <nik.reiman@gmail.com>
2013-03-15 19:52:35 +0100

    Add new effect classes, base to project

---
+++ Source/BufferProcessor.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    BufferProcessor.h
+    Created: 15 Mar 2013 7:51:14pm
+    Author:  nik
+
+  ==============================================================================
+*/
+
+#ifndef __BUFFERPROCESSOR_H_8B7D07F4__
+#define __BUFFERPROCESSOR_H_8B7D07F4__
+
+
+
+
+
+#endif  // __BUFFERPROCESSOR_H_8B7D07F4__
