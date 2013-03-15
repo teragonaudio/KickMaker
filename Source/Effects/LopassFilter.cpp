d17eb8c4298e70684e77717e3ac564f97495a347
Nik Reiman <nik.reiman@gmail.com>
2013-03-15 21:34:55 +0100

    Move lopass filter algorithm from old source code

--- Source/Effects/LopassFilter.cpp
+++ Source/Effects/LopassFilter.cpp
@@ -8,4 +8,37 @@
   ==============================================================================
 */
 
+#include <math.h>
 #include "LopassFilter.h"
+
+LopassFilter::LopassFilter() : BufferProcessor()
+{
+    inputs0 = inputs1 = inputs2 = 0.0f;
+    outputs1 = outputs2 = 0.0f;
+    coefficientA1 = coefficientA2 = 0.0f;
+    coefficientB1 = coefficientB2 = 0.0f;
+}
+
+void LopassFilter::process(float* buffer, int numSamples)
+{
+    for(int i = 0; i < numSamples; ++i) {
+        inputs2 = inputs1;
+        inputs1 = inputs0;
+        inputs0 = buffer[i];
+
+        buffer[i] = (coefficientA1 * inputs0) + (coefficientA2 + inputs1) + (coefficientA1 * inputs2) -
+            (coefficientB1 * outputs1) - (coefficientB2 * outputs2);
+
+        outputs2 = outputs1;
+        outputs1 = buffer[i];
+    }
+}
+
+void LopassFilter::calculateCoefficients()
+{
+    float c = 1.0f / tan(M_PI * frequency * sampleRate);
+    coefficientA1 = 1.0f / (1.0f + (resonance * c) + (c * c));
+    coefficientA2 = 2.0f * coefficientA1;
+    coefficientB1 = 2.0f * coefficientA1 * (1.0f - (c * c));
+    coefficientB2 = coefficientA1 * (1.0f - (resonance * c) + (c * c));
+}
--- Source/Effects/LopassFilter.h
+++ Source/Effects/LopassFilter.h
@@ -11,8 +11,32 @@
 #ifndef __LOPASSFILTER_H_91D21A5E__
 #define __LOPASSFILTER_H_91D21A5E__
 
+#include "../BufferProcessor.h"
 
+class LopassFilter : public BufferProcessor {
+public:
+    LopassFilter();
+    virtual ~LopassFilter() {}
 
+    void process(float* buffer, int numSamples);
 
+    void setFrequency(float inFrequency) { frequency = inFrequency; calculateCoefficients(); }
+    void setResonance(float inResonance) { resonance = inResonance; calculateCoefficients(); }
+    void setSampleRate(double inSampleRate) { sampleRate = inSampleRate; calculateCoefficients(); }
+
+private:
+    void calculateCoefficients();
+
+private:
+    double sampleRate;
+    float frequency;
+    float resonance;
+
+    float inputs0, inputs1, inputs2;
+    float outputs1, outputs2;
+
+    float coefficientA1, coefficientA2;
+    float coefficientB1, coefficientB2;
+};
 
 #endif  // __LOPASSFILTER_H_91D21A5E__
