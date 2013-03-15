a498d6dcf37386135186866ebf213e4a4dc1ba65
Nik Reiman <nik.reiman@gmail.com>
2013-03-15 08:05:40 +0100

    Sketch out some methods of Oscillator class

--- Source/Oscillator.cpp
+++ Source/Oscillator.cpp
@@ -9,3 +9,73 @@
 */
 
 #include "Oscillator.h"
+#include "NoiseWave.h"
+#include "SawtoothWave.h"
+#include "SineWave.h"
+#include "SquareWave.h"
+#include "TriangleWave.h"
+
+Oscillator::Oscillator()
+{
+    waveforms[0] = new SineWave();
+    waveforms[1] = new SquareWave();
+    waveforms[2] = new TriangleWave();
+    waveforms[3] = new SawtoothWave();
+    waveforms[4] = new NoiseWave();
+    currentWaveform = waveforms[0];
+}
+
+Oscillator::~Oscillator()
+{
+    for(int i = 0; i < kNumWaveforms; ++i) {
+        delete waveforms[i];
+    }
+}
+
+void Oscillator::observeParameters(PluginParameterSet& parameters)
+{
+    for(int i = 0; i < parameters.size(); ++i) {
+        parameters[i]->addObserver(this);
+    }
+}
+
+void Oscillator::onParameterUpdated(const PluginParameter* parameter)
+{
+    switch(parameter->getType()) {
+        case kTypeOscOn:
+            enabled = parameter->getValue();
+            break;
+        case kTypeOscType:
+            currentWaveform = waveforms[(int)parameter->getValue()];
+            break;
+        case kTypeOscDetune:
+            break;
+        case kTypeOscDetuneFine:
+            break;
+        case kTypeOscExpand:
+            break;
+        case kTypeOscExpandRate:
+            break;
+        case kTypeOscPhase:
+            break;
+        case kTypeOscAttack:
+            break;
+        case kTypeOscAttackAmp:
+            break;
+        case kTypeOscDecay:
+            break;
+        case kTypeOscDecayAmp:
+            break;
+        case kTypeOscSustain:
+            break;
+        case kTypeOscSustainAmp:
+            break;
+        case kTypeOscRelease:
+            break;
+        case kTypeOscVolume:
+            break;
+        default:
+            volume = parameter->getValue();
+            break;
+    }
+}
--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -11,8 +11,44 @@
 #ifndef __OSCILLATOR_H_A19FA352__
 #define __OSCILLATOR_H_A19FA352__
 
+#include "PluginParameters.h"
+#include "Waveform.h"
 
+using namespace teragon;
 
+static const int kNumWaveforms = 5;
 
+enum {
+    kTypeOscOn,
+    kTypeOscType,
+    kTypeOscDetune,
+    kTypeOscDetuneFine,
+    kTypeOscExpand,
+    kTypeOscExpandRate,
+    kTypeOscPhase,
+    kTypeOscAttack,
+    kTypeOscAttackAmp,
+    kTypeOscDecay,
+    kTypeOscDecayAmp,
+    kTypeOscSustain,
+    kTypeOscSustainAmp,
+    kTypeOscRelease,
+    kTypeOscVolume
+};
+
+class Oscillator : public PluginParameterObserver {
+public:
+    Oscillator();
+    virtual ~Oscillator();
+
+    void observeParameters(PluginParameterSet& parameters);
+    void onParameterUpdated(const PluginParameter* parameter);
+
+private:
+    Waveform* waveforms[kNumWaveforms];
+    Waveform* currentWaveform;
+    bool enabled;
+    float volume;
+};
 
 #endif  // __OSCILLATOR_H_A19FA352__
