4191c151616fdd242a6499f621c2a1f00fa52ab9
Nik Reiman <nik.reiman@gmail.com>
2013-03-15 21:34:35 +0100

    Move compressor algorithm over from old source code

--- Source/Effects/Compressor.cpp
+++ Source/Effects/Compressor.cpp
@@ -9,3 +9,38 @@
 */
 
 #include "Compressor.h"
+
+void Compressor::process(float* buffer, int numSamples)
+{
+    double keyDb;
+    double overDb;
+    double gain;
+
+    for(int i = 0; i < numSamples; ++i) {
+        keyDb = linear2dB(fabs(buffer[i]) + kDcOffset);
+        overDb = keyDb - threshold;
+        if(overDb < 0.0) {
+            overDb = 0.0;
+        }
+
+        overDb += kDcOffset;
+        if(envDb > overDb) {
+            envDb = overDb + attackCoefficient * (envDb - overDb);
+        }
+        else {
+            envDb = overDb + releaseCoefficient * (envDb - overDb);
+        }
+
+        // Subtract the DC offset again to avoid denormalization
+        overDb = envDb - kDcOffset;
+        gain = db2linear(overDb * (ratio - 1.0f));
+
+        buffer[i] *= gain;
+    }
+}
+
+void Compressor::calculateCoefficients()
+{
+    attackCoefficient = exp(-1000.0 / (attack * sampleRate));
+    releaseCoefficient = exp(-1000.0 / (release * sampleRate));
+}
--- Source/Effects/Compressor.h
+++ Source/Effects/Compressor.h
@@ -11,8 +11,44 @@
 #ifndef __COMPRESSOR_H_A61B1E37__
 #define __COMPRESSOR_H_A61B1E37__
 
+#include <math.h>
+#include "../BufferProcessor.h"
 
+class Compressor : public BufferProcessor {
+public:
+    Compressor() : BufferProcessor(), envDb(kDcOffset) {}
+    virtual ~Compressor() {}
 
+    void process(float* buffer, int numSamples);
 
+    void setAttack(double inAttack) { attack = inAttack; calculateCoefficients(); }
+    void setRelease(double inRelease) { release = inRelease; calculateCoefficients(); }
+    void setThreshold(double inThreshold) { threshold = inThreshold; }
+    void setRatio(double inRatio) { ratio = inRatio; }
+    void setSampleRate(double inSampleRate) { sampleRate = inSampleRate; calculateCoefficients(); }
+
+private:
+    static const double kDcOffset = 1E-25;
+    static double linear2dB(const double linear) {
+        return log(linear) * 8.685889638; // 20 / lin(10)
+    }
+    static double db2linear(const double decibels) {
+        return exp(decibels * 0.115129255); // ln(10) / 20
+    }
+
+    void calculateCoefficients();
+
+private:
+    double sampleRate;
+    double envDb;
+
+    float attack;
+    float release;
+    float threshold;
+    float ratio;
+
+    double attackCoefficient;
+    double releaseCoefficient;
+};
 
 #endif  // __COMPRESSOR_H_A61B1E37__
